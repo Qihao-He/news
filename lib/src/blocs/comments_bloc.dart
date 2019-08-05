20e4f7eb2d115f1231cd3c4e6b9c170f9cd69038
Qihao He <qi.he@maine.edu>
2019-08-05 15:29:41 -0700

    Recursive Data Fetching

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -4,14 +4,31 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class CommentsBloc {
-  final _commentsFetcher = PublishSubject<int>（）；
+  final _commentsFetcher = PublishSubject<int>();
   final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+  final _repository = Repository();
 
   // Streams
   Observable<Map<int, Future<ItemModel>>> get itemWithComments =>  _commentsOutput.stream;
 
   // Sink
   Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  CommentsBloc() {
+    _commentsFetcher.stream.tranform(_commentsTransformer()).pipe(_commentsOutput);
+  }
+
+  _commentsTransformer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+      (cache, int id, index) {
+        cache[id] = _repository.fetchItem(id);
+        cache[id].then((ItemModel item) {
+          item.kids.forEach((kidId) => fetchItemWithComments(kidId));
+        });
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
 
   dispose() {
     _commentsFetcher.close();
